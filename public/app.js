f6acaa761b7dc45b2967893d290489f8b078830d
MichaelMacLeod <edinburghmacleod@gmail.com>
2016-02-02 20:44:35 +0000

    Population now outputs with commas inside long numbers

--- public/app.js
+++ public/app.js
@@ -1,22 +1,52 @@
-window.onload = function(){
-  console.log('App started');
-  var url = 'https://restcountries.eu/rest/v1'
-  var request = new XMLHttpRequest();
+var countries = [] 
 
-//We want to make a GET request to our URL, with this.
-  request.open("GET", url)
-;
-//Now start defining the asynchronous actions for what we want to do when we have the info being sent back to us from the request object: 
+var loadCountries = function(){
 
-request.onload = function(){
-  if(request.status === 200){
-    console.log("Got the data!");
-    // console.log(request.responseText);
-    var useful = JSON.parse(request.responseText);
-    console.log(useful[0].name);
+  for (var i = 0; i < countries.length;  i++){
+  var countryName = document.createElement("option");
+  countryName.innerText = countries[i].name;
+  
+  var countryList = document.querySelector("select");
+  countryList.appendChild(countryName);
   }
 }
 
-request.send(null);
+var selectedName = function(){
+  //Json save to local storage and string. Parse and select name... look at var for film of films.
+
+  var name = document.getElementById("s1").value
+  var result = _.find(countries, function(o){return o.name === name;})
+ 
+var countryTitle = document.querySelector(".country-name");
+countryTitle.innerText = result.name;
+
+var countryPopulation = document.querySelector(".population");
+countryPopulation.innerText = "Population: " + Number( result.population ).toLocaleString();;
+
+var countryCapital = document.querySelector(".capital");
+countryCapital.innerText = "Capital city: " +result.capital;
+
+}
+
+
+window.onload = function(){  
+  console.log('App started');
+  var url = 'https://restcountries.eu/rest/v1';
+  var request = new XMLHttpRequest();
+  request.open("GET", url)
+
+  request.onload = function(){
+    if(request.status === 200){
+      console.log('got the data')
+      //console.log(request.responseText)
+      countries = JSON.parse(request.responseText );
+     
+      loadCountries();
+      selected = document.getElementById("s1")
+      selected.onchange = selectedName;
+    }
+  }
+
+  request.send(null)
 
 };
